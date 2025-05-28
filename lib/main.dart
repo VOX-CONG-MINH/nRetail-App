f16672bc28d76f8f79c340495d4754ce523c8c8d
MinhVC <213.minhvc@gmail.com>
2025-05-28 13:44:12 +0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class nRetailApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
